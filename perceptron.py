2769f5c60ea697736d5341054b58c952afea6510
Abhiram <anukoolaabhiram@outlook.com>
2025-09-28 17:57:20 +0530

    Refactor Value class and remove unnecessary operator classes
    - Simplified the Value class by removing operator classes (Tanh, Add, Subtract, Multiply, Divide).
    - Updated arithmetic operations to directly assign string representations of operations.
    - Introduced a children attribute to track operands for each operation.
    - Cleaned up the __str__ method for better readability.

--- perceptron.py
+++ perceptron.py
@@ -1,121 +1,43 @@
 import math
-import numpy  as np
-
-
-class Operator:
-    def __init__(self,op):
-        self.op = op
-
-
-class Tanh(Operator):
-    def __init__(self,input):
-        super().__init__("Tanh")
-        self.input = input
-
-
-    def __str__(self):
-        input = None
-        if self.input.op != None:
-            input = self.input.op
-        else:
-            input = self.input
-
-
-        return f"(tanh({input}))"
-
-class BinaryOp(Operator):
-    def __init__(self, op,left,right):
-        super().__init__(op)
-        self.left = left
-        self.right = right
-
-    def __str__(self):
-        left = None
-        right = None
-
-
-        if self.left.op != None:
-            left = self.left.op
-        else:
-            left = self.left
-
-        if self.right.op != None:
-            right = self.right.op
-        else:
-            right = self.right
-
-        return f"({left} {self.op} {right})"
-
-
-class Add(BinaryOp):
-    def __init__(self, l, r):
-        super().__init__("+",l,r)
-
-
-class Subtract(BinaryOp):
-    def __init__(self, l, r):
-        super().__init__("-",l,r)
-
-
-class Multiply(BinaryOp):
-    def __init__(self,l,r):
-        super().__init__("*",l,r)
-
-
-class Divide(BinaryOp):
-    def __init__(self,l,r):
-        super().__init__("/",l,r)
-    
-    
 
 class Value:
-    op = None
     def __init__(self,value):
         self.value = value
+        self.children = []
+        self.op = None
 
     def __add__(self,other):
         v = Value(self.value + other.value)
-        v.op = Add(self,other)
+        v.op = "+"
+        v.children = [self,other]
         return v
 
     def __radd__(self,other):
         v = Value(self.value + other)
-        v.op = Add(self,Value(other))
-        return v
-    
-    def __sub__(self,other):
-        v = Value(self.value - other.value)
-        v.op = Subtract(self,other)
+        v.op = "+"
+        v.children = [self,Value(other)]
         return v
     
     def __mul__(self,other):
         v = Value(self.value * other.value)
-        v.op = Multiply(self,other)
+        v.op = "*"
+        v.children = [self,other]
         return v
 
     def  __rmul__(self,other):
         v = Value(self.value * other)
-        v.op = Multiply(self,Value(other))
+        v.op = "*"
+        v.children = [self,Value(other)]
         return v
 
-    def __truediv__(self,other):
-        v = Value(self.value * other.value)
-        v.op = Divide(self,other)
-        return v
 
     def tanh(self):
         v = Value(math.tanh(self.value))
-        v.op = Tanh(self)
+        v.op = "tanh(x)" 
+        v.children = [self]
         return v
 
     def __str__(self):
-        left = None
-        if self.op != None:
-            left = str(self.op)
-        
-        if left != None:
-            return f"{left} = {str(self.value)}"
-
         return str(self.value) 
 
 
